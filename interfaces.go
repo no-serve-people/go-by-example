b145f15d4ed8ad3f4df25c2a3082cebb4fa868ca
ixingjue <826739558@qq.com>
2019-04-09 13:17:48 +0800

    添加接口

---
+++ interfaces.go
@@ -0,0 +1,48 @@
+// _接口_ 是方法特征的命名集合。
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// 这里是一个几何体的基本接口。
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+// 在我们的例子中，我们将让 `square` 和 `circle` 实现
+// 这个接口
+type square struct {
+	width, height float64
+}
+type circle struct {
+	radius float64
+}
+
+// `circle` 的实现。
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+// 如果一个变量的是接口类型，那么我们可以调用这个被命名的
+// 接口中的方法。这里有一个一通用的 `measure` 函数，利用这个
+// 特性，它可以用在任何 `geometry` 上。
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
+func main() {
+	s := square{width: 3, height: 4}
+	c := circle{radius: 5}
+	// 结构体类型 `circle` 和 `square` 都实现了 `geometry`
+	// 接口，所以我们可以使用它们的实例作为 `measure` 的参数。
+	measure(s)
+	measure(c)
+}
