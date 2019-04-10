326dfe96616011bc4494ed2b7c7b2a295546fbdd
ixingjue <826739558@qq.com>
2019-04-10 13:17:59 +0800

    fix

--- tickers.go
+++ tickers.go
@@ -9,7 +9,7 @@ func main() {
 	// 打点器和定时器的机制有点相似：一个通道用来发送数据。
 	// 这里我们在这个通道上使用内置的 `range` 来迭代值每隔
 	// 500ms 发送一次的值。
-	ticker := time.NewTicker(time.Microsecond * 500)
+	ticker := time.NewTicker(time.Millisecond * 500)
 	go func() {
 		for t := range ticker.C {
 			fmt.Println("Tick at", t)
